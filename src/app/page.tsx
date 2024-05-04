f2f71bb8cc2e381f9d1228cae0dd88b569b85a03
rhyanzp <84288591+rhyanzp@users.noreply.github.com>
2024-05-04 10:24:10 +0800

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <div className="text-center m-5">
+        <h1>Title - Display 360 Gem</h1>
+      </div>
+    </main>
+  );
+}
