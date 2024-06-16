90206d1145566dfa8ab9f6fdbd7a56d4694827a9
rhyanzp <84288591+rhyanzp@users.noreply.github.com>
2024-06-16 11:23:39 +0800

    prelim setup

---
+++ components/displayer.tsx
@@ -0,0 +1,8 @@
+function Displayer () {
+return (
+    <div className="tw-h-100 tw-w-100 tw-bg-w">
+        <h1>HELLO I AM THE DISPLAYER</h1>
+    </div>
+)
+}
+export default Displayer
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
-
+import Displayer from "../../components/displayer";
 export default function Home() {
   return (
     <main>
       <div className="text-center m-5">
-        <h1>Title - Display 360 Gem</h1>
+        <h1>Title - Display 360 Video</h1>
+        <Displayer></Displayer>
       </div>
     </main>
   );
